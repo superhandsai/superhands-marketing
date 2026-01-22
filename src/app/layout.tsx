670400e94abe63cdb08052aec1ae86bad38fbfd7
grant <grant@superhands.ai>
2026-01-22 14:13:30 +0000

    dm77755n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { PostHogProvider } from "@/providers/posthog-provider";
@@ -47,6 +48,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const intercomAppId = process.env.NEXT_PUBLIC_INTERCOM_APP_ID;
+
   return (
     <html lang="en" className={inter.variable}>
       <body className={`font-sans antialiased`}>
@@ -55,6 +58,30 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </PostHogProvider>
+        {intercomAppId && (
+          <>
+            <Script
+              id="intercom-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.intercomSettings = {
+                    app_id: "${intercomAppId}"
+                  };
+                `,
+              }}
+            />
+            <Script
+              id="intercom-script"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/${intercomAppId}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+                `,
+              }}
+            />
+          </>
+        )}
       </body>
     </html>
   );
