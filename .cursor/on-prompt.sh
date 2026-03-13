54dfb9d24905f7f1f903c2c03838fc7a0e6bc05d
Superhands <grant@superhands.ai>
2026-03-13 13:38:29 +0000

    Revert "Update Visual editor for PRs label to test"
    This reverts commit 81868c30e47b10100fbc31d89cb5d6c996126f14.

--- .cursor/on-prompt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-INPUT=$(cat)
-curl -s -X POST "http://127.0.0.1:43821/cursor-prompt" -H "Content-Type: application/json" -d "$INPUT" > /dev/null 2>&1 || true
-echo '{"permission":"allow"}'
-
--- .cursor/on-stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-INPUT=$(cat)
-curl -s -X POST "http://127.0.0.1:43821/cursor-commit" -H "Content-Type: application/json" -d "$INPUT" > /dev/null 2>&1 || true
-
